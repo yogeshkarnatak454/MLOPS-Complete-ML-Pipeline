614d3dbc0871eaa40b72c8cebdc8c474dac7b7f5
yogesh karnatak <yogesh454@gmail.com>
2026-06-06 13:01:14 +0530

    params configured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -89,8 +89,8 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        # test_size = params['data_ingestion']['test_size']
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
         test_size = 0.21
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
         df = load_data(data_url=data_path)
